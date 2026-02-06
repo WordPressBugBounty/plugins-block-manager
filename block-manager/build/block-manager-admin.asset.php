549b37a8ac6b7f69dfea3176745bde95b7516f52
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 04:33:38 +0000

    3.2.0

--- block-manager/build/block-manager-admin.asset.php
+++ block-manager/build/block-manager-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ae9ca091559004509b10');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ab956ecb1a4d4dbc7601');
